c96affbb6550f1977048b20b37ffa685101737f2
Rodrigoo14 <rodrigo.mechan@ucsm.edu.pe>
2022-08-24 23:38:02 -0500

    Se agrego el programa que pide realizar en las actividades del laboratorio nro 01, este programa nos da tres intentos para adivinar un numero y nos muestra un mensaje en caso lo descubramos o en caso no lo hagamos.

---
+++ Principal_01.java
@@ -0,0 +1,26 @@
+package Proyecto;
+
+import java.util.Scanner;
+
+public class Principal_01 {
+
+	public static void main(String[] args) {
+		int vida, rp;
+		boolean fl = false;
+		vida = 0;
+		rp=0;
+		Scanner sc = new Scanner(System.in);
+		do {
+			vida ++;
+			System.out.println("Vida: "+ vida +"\t¿Cual es el numero secreto? ");
+			rp = sc.nextInt();
+			if (rp == 1234)
+				fl = true;
+		} while(vida < 3 && !fl);
+		if (fl == true)
+			System.out.println("Adivinaste!!!!! :)");
+		else
+			System.out.println("Uppps, perdiste... :c ");
+		}
+
+}
